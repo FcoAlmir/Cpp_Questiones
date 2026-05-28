fa43f77cf66fcc8cdd62020bd15239c22c6e3464
FcoAlmir <falmirbl@gmail.com>
2026-05-28 10:17:45 -0300

    Add: 4A_Watermelon | 1_Two_Sum

--- LeetCode/1_Two_Sum.cpp
+++ LeetCode/1_Two_Sum.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <algorithm>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size() - 1, sum;
+        vector<int> res;
 
-int main() {
+        while(i <= j) {
+            sum = nums[i] + nums[j];
+            if(sum > target) {
+                if(nums[j] > target) j--;
+            }
 
-    return 0;
-}
+            else if(sum == target) {
+                res.push_back(i);
+                res.push_back(j);
+                return res;
+            }
+        }
+        return res;
+    }
+};
